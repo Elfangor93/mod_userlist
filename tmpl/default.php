a6983a9d1d9f448566a8841fb04ca1ea955ae6fb
19leunam93 <39154009+19leunam93@users.noreply.github.com>
2018-06-18 21:55:07 +0200

    fixed URL to PDF

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,12 +6,13 @@ defined('_JEXEC') or die; ?>
 // HTML-Output
 // ------------
 $output = '';
+$url = JUri::base() . 'modules/mod_userlist/userlist_PDF.php';
 ?>
 
 <div>    
 	<div class="table-responsive">  
 		<div class="col-md-12" align="right">
-	    	<form id="form_PDF" method="post" action="modules/mod_userlist/userlist_PDF.php ">
+	    	<form id="form_PDF" method="post" action="<?php echo $url;?>">
 	        	<input type="submit" name="generate_pdf" class="btn btn-success" formtarget="_blank" value="PDF Download">  
 	     	</form>  
 	    </div>
@@ -19,16 +20,41 @@ $output = '';
 	    <br/>
 <?php
 
+// define Table-Style
+$output .= '<style>
+				table {
+				    font-family: arial, sans-serif;
+				    border: 2px solid #000000;
+				    border-collapse: collapse;
+				    width: 100%;
+				}
+
+				td, th {
+				    border: 1px solid #000000;
+				    text-align: left;
+				    padding: 8px;
+				}
+
+				tr:nth-child(even) {
+				    background-color: #dddddd;
+				}
+
+				.table_heading {
+					font-weight: bold;
+					border: 2px solid #000000;
+				}
+		    </style>';
+
 // Create Table-Header
-$output .= '<table class="table table-bordered">
-	        	<tr>
-	               	<th width="20%">Name</th>
+$output .= '<table border="1">
+	        	<tr class="table_heading">
+	               	<th width="15%">Name</th>
 	                <th width="10%">Benutzer</th> 
-	                <th width="20%">E-Mail</th>
-	                <th width="5%">Frei-gegeben</th>   
-	                <th width="5%">Aktiviert</th>
-	                <th width="20%">Letzter Besuch</th>
-	                <th width="20%">Register-Datum</th> 
+	                <th width="25%">E-Mail</th>
+	                <th width="10%">Frei-gegeben</th>   
+	                <th width="10%">Aktiviert</th>
+	                <th width="15%">Letzter Besuch</th>
+	                <th width="15%">Register-Datum</th> 
 	        	</tr>';
 
 // Create Table out of fetched Data from Database
