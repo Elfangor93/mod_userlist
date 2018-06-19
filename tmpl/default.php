861625491c88d5623240e60e87da34bbcbec2d8c
19leunam93 <39154009+19leunam93@users.noreply.github.com>
2018-06-19 23:41:16 +0200

    fixed issue #2

--- tmpl/default.php
+++ tmpl/default.php
@@ -9,54 +9,48 @@ $output = '';
 $url = JUri::base() . 'modules/mod_userlist/userlist_PDF.php';
 ?>
 
+<style>
+	table {
+	    font-family: arial, sans-serif;
+	    border: 2px solid #000000;
+	    border-collapse: collapse;
+	    width: 100%;
+	}
+	td, th {
+	    border: 1px solid #000000;
+	    text-align: left;
+	    padding: 8px;
+	}
+	tr:nth-child(even) {
+	    background-color: #dddddd;
+	}
+	.table_heading {
+		font-weight: bold;
+		border: 2px solid #000000;
+	}
+</style>
+
 <div>    
 	<div class="table-responsive">  
 		<div class="col-md-12" align="right">
-	    	<form id="form_PDF" method="post" action="<?php echo $url;?>">
-	        	<input type="submit" name="generate_pdf" class="btn btn-success" formtarget="_blank" value="PDF Download">  
+	    	<form id="form_PDF" method="post" action="">
+	        	<input type="submit" name="generate_pdf" class="btn btn-success" value="PDF Download">  
 	     	</form>  
 	    </div>
 	    <br/>
 	    <br/>
+	    <table border="1">
+        	<tr class="table_heading">
+               	<th width="15%">Name</th>
+                <th width="10%">Benutzer</th> 
+                <th width="25%">E-Mail</th>
+                <th width="10%">Frei-gegeben</th>   
+                <th width="10%">Aktiviert</th>
+                <th width="15%">Letzter Besuch</th>
+                <th width="15%">Register-Datum</th> 
+        	</tr>
+
 <?php
-
-// define Table-Style
-$output .= '<style>
-				table {
-				    font-family: arial, sans-serif;
-				    border: 2px solid #000000;
-				    border-collapse: collapse;
-				    width: 100%;
-				}
-
-				td, th {
-				    border: 1px solid #000000;
-				    text-align: left;
-				    padding: 8px;
-				}
-
-				tr:nth-child(even) {
-				    background-color: #dddddd;
-				}
-
-				.table_heading {
-					font-weight: bold;
-					border: 2px solid #000000;
-				}
-		    </style>';
-
-// Create Table-Header
-$output .= '<table border="1">
-	        	<tr class="table_heading">
-	               	<th width="15%">Name</th>
-	                <th width="10%">Benutzer</th> 
-	                <th width="25%">E-Mail</th>
-	                <th width="10%">Frei-gegeben</th>   
-	                <th width="10%">Aktiviert</th>
-	                <th width="15%">Letzter Besuch</th>
-	                <th width="15%">Register-Datum</th> 
-	        	</tr>';
-
 // Create Table out of fetched Data from Database
 foreach ($items as $key => $entry) {
 	$output .= '<tr>
@@ -78,3 +72,37 @@ foreach ($items as $key => $entry) {
 		</textarea>
 	</div>
 </div>
+
+<script>
+jQuery(document).ready(function(){
+
+	jQuery("#form_PDF").submit(function(event) {
+	    event.preventDefault();
+
+		var formData = new FormData(document.getElementById('form_PDF'));
+		//formData.append('appended1', 'appended value');
+
+		var xhr=new XMLHttpRequest();
+		xhr.open("POST", "<?php echo $url;?>", true);
+		xhr.responseType = 'blob';
+
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var blob = xhr.response;
+			    var link = document.createElement('a');
+			    document.body.appendChild(link);
+			    link.href = URL.createObjectURL(blob);
+			    link.download="Benutzerliste.pdf";
+				link.style = "display: none";
+			    link.click();
+			} else if (this.status == 403) {
+				alert("Antwort vom Server: 403 - Access denied");
+			} else {
+				console.log(this.readyState);
+				console.log(this.status);
+			}
+		};
+		xhr.send(formData);
+	});
+});
+</script>
